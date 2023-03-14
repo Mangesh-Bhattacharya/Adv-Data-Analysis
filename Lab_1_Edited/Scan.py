82a57ff7fce76ef496e7960f49eb48f0217f9f06
Mangesh Bhattacharya <codeoriginals.20@gmail.com>
2023-03-14 12:49:31 -0400

    commit

--- Lab_1_Edited/Scan.py
+++ Lab_1_Edited/Scan.py
@@ -5,7 +5,7 @@ import subprocess as sp  # import the subprocess module as sp
 
 # Path to the directory containing the files to be scanned
 # Change this to the path where you have stored the files
-files = "/home/mangesh/Downloads/malware_data_science/ch8/data/benignware/agentsvr.exe"
+files = "/home/user/Downloads/malware_data_science/ch8/data/benignware/agentsvr.exe"
 # MD5 hash of the file to be generated using the md5sum command
 # run the md5sum command and store the output in stream variable
 stream = os.popen("md5sum" + files)
@@ -17,7 +17,7 @@ hvalue = hvalue.split()
 # print the hash value to the console for debugging
 print("Hash Value is: " + str(hvalue))
 # run the capa command and store the output in process variable
-proc = sp.Popen(["/home/mangesh/Downloads/capa", "-v", files], stdout=sp.PIPE, universal_newlines=True)
+proc = sp.Popen(["/home/user/Downloads/capa", "-v", files], stdout=sp.PIPE, universal_newlines=True)
 # get the output of the command and store it in output variable and error in error variable
 output, error = proc.communicate()
 # print the output to the console for debugging
